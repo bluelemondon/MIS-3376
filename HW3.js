a8603d01aed8e4a62f611adfe5e08fe9a0b5fa3b
BlueLemon <rayray5159@gmail.com>
2025-04-13 23:28:43 -0500

    Update HW3.js

--- HW3.js
+++ HW3.js
@@ -245,30 +245,7 @@ function reviewdata() {
             }
         }
     }
-    function reviewInput() {
-    var formcontent = document.getElementById("careform");
-    var formoutput = "<table class='output'><th colspan = '3'> Review Your Information:</th>";
-    for (let i = 0; i < formcontent.length; i++) {
-        if (formcontent.elements[i].value !== "") {
-            switch (formcontent.elements[i].type) {
-                case "checkbox":
-                    if (formcontent.elements[i].checked) {
-                        formoutput += `<tr><td align='right'>${formcontent.elements[i].name}</td><td>&#x2713;</td></tr>`;
-                    }
-                    break;
-                case "radio":
-                    if (formcontent.elements[i].checked) {
-                        formoutput += `<tr><td align='right'>${formcontent.elements[i].name}</td><td>${formcontent.elements[i].value}</td></tr>`;
-                    }
-                    break;
-                default:
-                    formoutput += `<tr><td align='right'>${formcontent.elements[i].name}</td><td>${formcontent.elements[i].value}</td></tr>`;
-            }
-        }
-    }
-    formoutput += "</table>";
-    document.getElementById("showInput").innerHTML = formoutput;
-}
+    
     var iframe = document.getElementById("reviewFrame");
     iframe.style.display = "block"; 
     // Show form output in iframe
