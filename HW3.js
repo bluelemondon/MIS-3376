1784dcb1e4b478afaebea7609f5dcc97a922dec3
BlueLemon <rayray5159@gmail.com>
2025-04-13 21:07:19 -0500

    Update HW3.js

--- HW3.js
+++ HW3.js
@@ -19,7 +19,60 @@ document.getElementById("today").innerHTML = new Date().toLocaleDateString();
     document.getElementById('DOB').setAttribute('max', maxDateString);
 
 //end of w3 schools
+function validateSsn() {
+    const ssn = document.getElementById("socialsec").value;
+    const ssnR = /^[0-9]{3}-?[0-9]{2}-?[0-9]{4}$/;
 
+    if (!ssnR.test(ssn)) {
+        document.getElementById("ssn-error").innerHTML = 
+        "Please enter a valid 9 digit SSN";
+        return false;
+    } else {
+        document.getElementById("ssn-error").innerHTML = "";
+        return true;
+    }
+}
+function validateDob() {
+    dob = document.getElementById("DOB");
+    let date = new Date(dob.value);
+    let maxDate = new Date().setFullYear(new Date().getFullYear() - 120);
+
+    if (date > new Date()) {
+        document.getElementById("dob-error").innerHTML = 
+        "Date can't be in the future";
+        dob.value = "";
+        return false;
+    } else if (date < new Date(maxDate)) {
+        document.getElementById("dob-error").innerHTML = 
+        "Invalid date(cannot be more than 120 years old";
+        dob.value = "";
+        return false;
+    } else {
+        document.getElementById("dob-error").innerHTML = "";
+        return true;
+    }
+}
+function validateZcode() {
+    const zipInput = document.getElementById("zip");
+    let zip = zipInput.value.replace(/[^\d-]/g, "");
+
+    if (!zip) {
+        document.getElementById("zcode-error").innerHTML = 
+        "Zip code can't be blank";
+        return false;
+    }
+
+    if (zip.length > 5) {
+        zip = zip.slice(0, 5) + "-" + zip.slice(5, 9);
+    } else {
+        zip = zip.slice(0, 5);
+    }
+
+    zipInput.value = zip;
+    document.getElementById("zcode-error").innerHTML = "";
+    return true;
+}
+var emailR = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 function show1() {
   var x = document.getElementById("socialsec");
   if (x.type === "password") {
@@ -28,6 +81,23 @@ function show1() {
     x.type = "password";
   }
 }
+function validateEmail() {
+    var email = document.getElementById("email").value;
+    var emailError = document.getElementById("emailError");
+    var emailR = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (email == "") {
+      emailError.innerHTML = "Email cannot be blank.";
+      return false;
+    } else if (!emailR.test(email)) {
+      emailError.innerHTML = "Please enter a valid email address.";
+      return false;
+    } else {
+      emailError.innerHTML = "";
+      alert("Email is valid!");
+      return true;
+    }
+  }
 
 function show2() {
   var x = document.getElementById("password");
